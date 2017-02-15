8d22d926c7e252305c08eda8c030711cf48b66eb
Louis Lie <Louis Lie>
2017-02-15 13:57:35 +0800

    add comment and add ()

--- Ch07/adaboost.py
+++ Ch07/adaboost.py
@@ -81,7 +81,7 @@ def adaBoostTrainDS(dataArr,classLabels,numIt=40):
         #print "aggClassEst: ",aggClassEst.T
         aggErrors = multiply(sign(aggClassEst) != mat(classLabels).T,ones((m,1))) #sign函数是符号函数如果x是负值则输出-1，如果是正值则是1
         errorRate = aggErrors.sum()/m
-        print "total error: ",errorRate
+        print("total error: ",errorRate)
         if errorRate == 0.0: break
     return weakClassArr,aggClassEst
 
@@ -94,7 +94,7 @@ def adaClassify(datToClass,classifierArr):
                                  classifierArr[i]['thresh'],\
                                  classifierArr[i]['ineq'])#call stump classify
         aggClassEst += classifierArr[i]['alpha']*classEst
-        print aggClassEst
+        print(aggClassEst)
     return sign(aggClassEst)
 
 def plotROC(predStrengths, classLabels):
@@ -122,4 +122,11 @@ def plotROC(predStrengths, classLabels):
     plt.title('ROC curve for AdaBoost horse colic detection system')
     ax.axis([0,1,0,1])
     plt.show()
-    print "the Area Under the Curve is: ",ySum*xStep
+    print("the Area Under the Curve is: ",ySum*xStep)
+
+if __name__ == '__main__':
+    dataArr,label = loadDataSet('E:\github\ml\Ch07\horseColicTraining2.txt')
+    classifierArray,aggClassEst = adaBoostTrainDS(dataArr,label,10)
+    plotROC(aggClassEst.T,label)
+
+
